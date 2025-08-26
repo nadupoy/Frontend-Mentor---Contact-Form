44d764d8eb5b6fc65286b3d23febf9e14d5932aa
nadupoy <gnsampao@gmail.com>
2025-08-26 11:27:16 +0300

    Deleted src/script.js.

--- src/main.js
+++ src/main.js
@@ -1,65 +1,9 @@
-import "./style.css";
-
-document.querySelector("#app").innerHTML = `
-  <main>
-  <h1>Contact Us</h1>
-
-  <form>
-  <section class="full-name">
-  <div class="first-name">
-  <label for="first-name">First Name <span>*</span></label>
-  <input type="text" id="first-name" name="first-name" required>
-  <p class="error-message">This field is required</p>
-  </div>
-
-  <div class="last-name">
-  <label for="last-name">Last Name <span>*</span></label>
-  <input type="text" id="last-name" name="last-name" required>
-  <p class="error-message">This field is required</p>
-  </div>
-  </section>
-
-  <section class="email-address">
-  <label for="email-address">Email Address <span>*</span></label>
-  <input type="email" id="email-address" name="email-address" required>
-  <p class="error-message">Please enter a valid email address</p>
-  </section>
-
-  <section class="query-type">
-  <p class="query">Query Type <span>*</span></p>
-  <div>
-  <div class="general-enquiry">
-  <input type="radio" id="general-enquiry" name="query-type" required>
-  <label for="general-enquiry">General Enquiry</label>
-  </div>
-
-  <div class="support-request">
-  <input type="radio" id="support-request" name="query-type" required>
-  <label for="support-request">Support Request</label>
-  </div>
-  </div>
-  <p class="error-message">Please select a query type</p>
-  </section>
-
-  <section class="message">
-  <label for="message">Message <span>*</span></label>
-  <textarea id="message" name="message" required></textarea>
-  <p class="error-message">This field is required</p>
-  </section>
-
-  <section class="consent">
-  <input type="checkbox" id="consent" name="consent" value="yes" required>
-  <label for="consent">I consent to being contacted by the team <span>*</span></label>
-  <p class="error-message">To submit this form please consent to being contacted</p>
-  </section>
-
-  <input type="submit" value="Submit">
-  </form>
-  </main>
-
-  <footer>
-  <p>
-  Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a> | Coded by <a href="https://github.com/nadupoy" target="_blank">Nadupoy</a>
-  </p>
-  </footer>
-`;
+// Users should be able to:
+//  - Complete the form and see a success toast message upon successful submission
+//  - Receive form validation messages if:
+//      - A required field has been missed
+//      - The email address is not formatted correctly
+//  - Complete the form only using their keyboard
+//  - Have inputs, error messages, and the success message announced on their screen reader
+//  - View the optimal layout for the interface depending on their device's screen size
+// - See hover and focus states for all interactive elements on the page
